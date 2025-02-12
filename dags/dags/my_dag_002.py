8c79117e5a28668505a641684f5628b29e7f3e28
Leo Barrientos C <leobarrientosc@edu.evolveacademy.es>
2025-02-12 19:58:03 +0100

    XCOM de DAG 2

---
+++ dags/dags/my_dag_002.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import random
+from datetime import datetime
+
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+# Diccionario de estilos de pintura y pintores
+pintores_por_estilo = {
+    "Impressionism": ["Claude Monet", "Edgar Degas", "Pierre-Auguste Renoir", "Mary Cassatt", "Camille Pissarro"],
+    "Post-Impressionism": ["Vincent van Gogh", "Paul Cézanne", "Paul Gauguin", "Henri de Toulouse-Lautrec", "Georges Seurat"],
+    "Expressionism": ["Edvard Munch", "Ernst Ludwig Kirchner", "Emil Nolde", "Franz Marc", "Egon Schiele"],
+    "Cubism": ["Pablo Picasso", "Georges Braque", "Juan Gris", "Fernand Léger", "Robert Delaunay"],
+    "Surrealism": ["Salvador Dalí", "René Magritte", "Joan Miró", "Max Ernst", "André Breton"],
+    "Pop Art": ["Andy Warhol", "Roy Lichtenstein", "James Rosenquist", "Claes Oldenburg", "Robert Rauschenberg"],
+    "Minimalism": ["Donald Judd", "Sol LeWitt", "Agnes Martin", "Dan Flavin", "Robert Morris"],
+    "Contemporary Art": ["Jeff Koons", "Damien Hirst", "Ai Weiwei", "Marina Abramović", "Takashi Murakami"]
+}
+
+def _obtener_pintores_y_estilo(**kwargs):
+    """
+    Selecciona aleatoriamente un estilo de pintura y dos pintores.
+    """
+    estilos = list(pintores_por_estilo.keys())
+    estilo = random.choice(estilos)
+    pintores_del_estilo = pintores_por_estilo[estilo]
+    pintores = random.sample(pintores_del_estilo, 2)
+    kwargs['ti'].xcom_push(key='estilo', value=estilo)
+    kwargs['ti'].xcom_push(key='pintor_1', value=pintores[0])
+    kwargs['ti'].xcom_push(key='pintor_2', value=pintores[1])
+
+def _prompt(**kwargs):
+    estilo = kwargs['ti'].xcom_pull(task_ids='obtener_estilo_y_pintores', key='estilo')
+    pintor_1 = kwargs['ti'].xcom_pull(task_ids='obtener_estilo_y_pintores', key='pintor_1')
+    pintor_2 = kwargs['ti'].xcom_pull(task_ids='obtener_estilo_y_pintores', key='pintor_2')
+    prompt =  "Generate an painting about " + estilo + " like " + pintor_1 + " or " + pintor_2 + "."
+    kwargs['ti'].xcom_push(key='prompt', value=prompt)
+
+with DAG(
+    dag_id="estilo_y_pintores_aleatorios",
+    start_date=datetime(2023, 10, 26),
+    schedule=None,
+    catchup=False,
+) as dag:
+    
+    start = EmptyOperator(task_id="inicio")
+
+    tarea_estilo_y_pintores = PythonOperator(
+        task_id="obtener_estilo_y_pintores",
+        python_callable=_obtener_pintores_y_estilo,
+        provide_context=True,
+    )
+
+    tarea_prompt = PythonOperator(
+        task_id="obtener_prompt",
+        python_callable=_prompt,
+        provide_context=True,
+    )
+
+
+
+    end = EmptyOperator(task_id="fin")
+
+    start >> tarea_estilo_y_pintores >> tarea_prompt >> end
